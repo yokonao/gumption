f2ba2f52d8c84d72dc9caf13a7166ec24ec2829d
yokonao <yokotukanao@me.com>
2020-09-14 16:30:46 +0900

    update: 関数をコールできていなかったので修正

--- chapter3/linked_list.cpp
+++ chapter3/linked_list.cpp
@@ -13,12 +13,12 @@ bool LinkedList::isEmpty() { return m_head.next == &m_head; }
 
 Item *LinkedList::first()
 {
-    assert(!isEmpty);
+    assert(!isEmpty());
     return m_head.next;
 }
 
 Item *LinkedList::last()
 {
-    assert(!isEmpty);
+    assert(!isEmpty());
     return m_head.prev;
 }
