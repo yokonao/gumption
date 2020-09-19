769c606660047c42d2816de5bbf6b945b7f3b8e9
yokonao <yokotukanao@me.com>
2020-09-19 10:46:15 +0900

    下準備

---
+++ chapter4_nao/ch4.1/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cassert>
+
+void test()
+{
+}
+
+int main()
+{
+    std::cout << "TEST START" << std::endl;
+    test();
+    std::cout << "ALL GREEN" << std::endl;
+    return 0;
+}
