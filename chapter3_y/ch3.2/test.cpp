784e5b62406be0d04f86222191a726b0a5fe03f3
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-09-21 19:29:06 +0900

    add : UArrayの実装

---
+++ chapter3_y/ch3.2/test.cpp
@@ -0,0 +1,5 @@
+#include "uarray.h"
+#include <iostream>
+using namespace std;
+
+
---
+++ chapter3_y/ch3.2/uarray.cpp
@@ -0,0 +1,36 @@
+#include "uarray.h"
+#include <cassert>
+using namespace std;
+
+UArray::UArray(int al, int be){
+    alpha = al;
+    beta = be;
+    b = new int[0];
+}
+int UArray::operator[](int i){
+    if(i >= 0 and i < n)
+    return b[i]; 
+}
+int UArray::size(){return n;}
+void UArray::pushBack(int e){
+    if(n == w){
+        reallocate(beta * n);
+    }
+    b[n] = e;
+    n++;
+}
+void UArray::popBack(){
+    assert(n>0);
+    n--;
+    if(alpha * n <= w and n > 0){
+        reallocate(beta * n);
+    }
+
+}
+void UArray::reallocate(int wp){
+    w = wp;
+    int *bp = new int[w];
+    for(int i;i<n;i++){bp[i] = b[i];}
+    delete b;
+    b = bp;
+}
