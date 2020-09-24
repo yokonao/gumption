4be56d1e1aea51761c8ba7931919c4360b677555
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-09-24 17:22:32 +0900

    add : 乱数生成器の作成

---
+++ chapter4_y/ch4.2/test_random.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "gen_random.h"
+#include "expectation.h"
+void testRandom()
+{
+    for (int m = 1; m < 10000; m++)
+    {
+        expect(gen_mt(m) < m).to_be_truthy();
+        expect(0 <= gen_mt(m)).to_be_truthy();
+    }
+    expect_error<std::invalid_argument>([] {
+        int n = gen_mt(0);
+    });
+    expect_error<std::invalid_argument>([] {
+        int n = gen_mt(-1);
+    });
+}
+int main()
+{
+    std::cout << "TEST START" << std::endl;
+    testRandom();
+    std::cout << "ALL GREEN" << std::endl;
+    return 0;
+}
