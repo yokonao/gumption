69bd0ed86254b8d99cdbd3035d360326b1290260
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-09-26 16:10:52 +0900

    add : ダミーの宣言

--- chapter4_y/ch4.2/uarray.h
+++ chapter4_y/ch4.2/uarray.h
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
-class UArray{
-    private:
-        int beta;
-        int alpha;
-        int w = 1;
-        int n = 0;
-        int *b;
-    public:
-        int operator [](int i);
-        int size();
-        void pushBack(int e);
-        void popBack();
-        void reallocate(int wp);
-        UArray(int al, int be);
+class UArray
+{
+private:
+    int beta;
+    int alpha;
+    int w = 1;
+    int n = 0;
+    int *b;
+
+public:
+    int operator[](int i);
+    int size();
+    void pushBack(int e);
+    void popBack();
+    void reallocate(int wp);
+    UArray();
+    UArray(int al, int be);
 };
