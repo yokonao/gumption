aecd3ce5b5bca418dc9c8ed480d060db949fa640
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-03 21:21:05 +0900

    add : hashとrandomの描画

--- chapter4_y/ch4.2/watch_hash.cpp
+++ chapter4_y/ch4.2/watch_hash.cpp
@@ -3,32 +3,53 @@
 #include "operation.h"
 #include "universal_hash.h"
 #include <uarray.h>
-
-int main()
+UArray<int> watch_cite()
 {
     int m = 257;
     int w = floorlog(m);
+
     UArray<int> res;
     for (int i = 0; i < m; i++)
     {
         res.pushBack(0);
     }
-    char c;
-    std::string cs;
     for (int rep = 0; rep < m * m; rep++)
     {
-        std::string ts = "";
+        std::string cString = "";
         for (int i = 0; i < 10; i++)
         {
-            c = 'a';
-            cs = "";
-            c = c + gen_mt(26);
-            cs = cs + c;
+            char c = 'a' + gen_mt(26); //aからzの文字を一つ作成
+            cString = cString + c;
         }
-        res[hash(key(cs, w), m)]++;
+        res[hash(key(cString, w), m)]++;
     }
-    for (int i = 0; i < m; i++)
+    return res;
+}
+double get_mean()
+{
+    int rep = 10;
+    int idx = 111;
+    double mean = 0.0;
+    UArray<int> res;
+    for (int i = 0; i < rep; i++)
+    {
+        std::cout << "start:" << i << std::endl;
+        res = watch_cite();
+        mean += res[idx];
+    }
+    mean /= rep;
+    return mean;
+}
+void print_hash()
+{
+    UArray<int> res;
+    res = watch_cite();
+    for (int i = 0; i < res.size(); i++)
     {
         std::cout << res[i] << std::endl;
     }
+}
+int main()
+{
+    print_hash();
 }
