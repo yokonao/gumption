6097c092c8d87d5932595287b86f4e811cf3adcc
yokonao <yokotukanao@me.com>
2020-10-05 08:03:30 +0900

    テスト落とす

--- lib/tests/test_s_linked_list.cpp
+++ lib/tests/test_s_linked_list.cpp
@@ -12,7 +12,7 @@ void testSItem()
     expect(item.e).to_be("a");
     expect(item.next).to_be(&dummy);
     expect(dummy.next).to_be(&item);
-    SItem item2("abcd", "", &dummy);
+    SItem item2("abcde", "", &dummy);
     dummy.next = &item;
     expect(item2.e).to_be("abcd");
     expect(item2.next).to_be(&dummy);
