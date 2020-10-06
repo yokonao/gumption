3f83682c88c591cf6c1bf1a9c7cae51b529c0831
yokonao <yokotukanao@me.com>
2020-10-06 22:43:48 +0900

    add: 1-万能なハッシュ関数群を表現するクラスを実装

---
+++ lib/hash_family_one_universal.h
@@ -0,0 +1,69 @@
+#include <string>
+#include <bit_string.h>
+#include <stdexcept>
+#include <cmath>
+#include <random>
+#include <iostream>
+
+int floorlog(int m)
+{
+    if (m < 1)
+    {
+        throw std::invalid_argument("1以上の値を指定してください");
+    }
+    return int(floor(log2(double(m))));
+}
+
+int gen_mt(int m)
+{
+    if (m <= 0)
+    {
+        throw std::invalid_argument("1以上の値を指定してください");
+    }
+    //[0,1]の一様分布を作成
+    std::mt19937 mt{std::random_device{}()};
+    std::uniform_int_distribution<int> dist(0, m - 1);
+    return dist(mt);
+}
+
+class HashFamily_OneUniversal
+{
+    int m;
+    int w;
+
+public:
+    HashFamily_OneUniversal(int m)
+    {
+        this->m = m;
+        this->w = floorlog(m);
+    }
+
+    int hash(std::string s)
+    {
+        BitString x = BitString(s);
+        int k = (w - 1 + x.size()) / w;
+        long long N = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            int x_i = 0;
+            for (int j = 0; j < w; j++)
+            {
+                int idx = i * w + j;
+                if (idx >= x.size())
+                {
+                    break;
+                }
+                x_i += x[idx] * pow(2, j);
+            }
+            int a_i = gen_mt(m);
+            std::cout << "a_i: " << a_i << std::endl;
+            std::cout << "x_i: " << x_i << std::endl;
+            std::cout << N << " + " << a_i * x_i << " = ";
+            N += x_i * a_i;
+            std::cout << N << std::endl;
+        }
+        
+        return N % m;
+    }
+};
--- lib/tests/test_hash_family_one_universal.cpp
+++ lib/tests/test_hash_family_one_universal.cpp
@@ -1,8 +1,17 @@
+#include "hash_family_one_universal.h"
 #include "expectation.h"
 
 int main()
 {
     std::cout << "TEST START" << std::endl;
+    HashFamily_OneUniversal ou = HashFamily_OneUniversal(257);
+    int tmp;
+    tmp = ou.hash("p");
+    std::cout << "p → " << tmp << std::endl;
+    tmp = ou.hash("ruby");
+    std::cout << "ruby → " << tmp << std::endl;
+    tmp = ou.hash("python");
+    std::cout << "python → " << tmp << std::endl;
     std::cout << "ALL GREEN" << std::endl;
 
     return 0;
