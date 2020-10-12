90cf3aabaa2f981e1d6a5611295b2fe063a1bb96
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-12 13:14:48 +0900

    add : selection sort実装

---
+++ src/chapter5/ch5.1/selection_sort.cpp
@@ -0,0 +1,33 @@
+#include <uarray.h>
+
+class SelectionSort
+{
+    void sort(UArray<int> a)
+    {
+        int min_idx, buff;
+        for (int i = 0; i < a.size(); i++)
+        {
+            min_idx = i;
+            for (int j = i; j < a.size(); j++)
+            {
+                if (a[j] < a[min_idx])
+                {
+                    min_idx = j;
+                }
+            }
+            if (min_idx == i)
+            {
+                continue;
+            }
+            else
+            {
+                for (int j = min_idx; j > i; j--)
+                {
+                    buff = a[j];
+                    a[j] = a[j - 1];
+                    a[j - 1] = buff;
+                }
+            }
+        }
+    }
+};
---
+++ src/chapter5/ch5.1/selection_sort.h
@@ -0,0 +1,9 @@
+#ifndef SELECTION_SORT
+#define SELECTION_SORT
+
+#include <uarray.h>
+class SelectionSort
+{
+    void sort(UArray<int> a);
+};
+#endif
