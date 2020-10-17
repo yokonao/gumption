b6653019ea6b17911105a947ae7c11b1c549635c
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-18 00:53:14 +0900

    add : good case test

--- src/chapter5/ch5.1/test_selection_sort.cpp
+++ src/chapter5/ch5.1/test_selection_sort.cpp
@@ -2,6 +2,16 @@
 #include "initial_list.h"
 #include <expectation.h>
 
+void test_SelectionSort_Good()
+{
+    int n = 10;
+    UArray<int> a = initial_list_good(n);
+    selection_sort(a);
+    for (int i = 1; i <= n; i++)
+    {
+        expect(a[i - 1]).to_be(i);
+    }
+}
 void test_SelectionSort_Worst()
 {
     int n = 10;
@@ -15,6 +25,7 @@ void test_SelectionSort_Worst()
 int main()
 {
     std::cout << "TEST START" << std::endl;
+    test_SelectionSort_Good();
     test_SelectionSort_Worst();
 
     std::cout << "ALL GREEN" << std::endl;
