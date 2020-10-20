a3f10d5eabc684000891c253b5970468c78b1256
yokonao <yokotukanao@me.com>
2020-10-20 21:15:58 +0900

    update: テストを簡略化

--- src/chapter5/test_sort.cpp
+++ src/chapter5/test_sort.cpp
@@ -2,200 +2,44 @@
 #include "initial_list.h"
 #include <expectation.h>
 
-void testSelectionSortGood()
+template <typename T>
+void testSort()
 {
     int n = 10;
     UArray<int> a = initial_list_good(n);
-    SelectionSort::execute(a);
+    T::execute(a);
     for (int i = 1; i <= n; i++)
     {
         expect(a[i - 1]).to_be(i);
     }
-}
-void testSelectionSortWorst()
-{
-    int n = 10;
-    UArray<int> a = initial_list_worst(n);
-    SelectionSort::execute(a);
+
+    n = 10;
+    a = initial_list_worst(n);
+    T::execute(a);
     for (int i = 1; i <= n; i++)
     {
         expect(a[i - 1]).to_be(i);
     }
-}
-void testSelectionSortRandom()
-{
-    for (int rep = 0; rep < 10; rep++)
-    {
-        int n = rep + 10;
-        UArray<int> a = initial_list_random(n);
-        SelectionSort::execute(a);
-        int prev = 0;
-        for (int i = 0; i < n; i++)
-        {
-            expect(a[i] >= prev).to_be_truthy();
-            prev = a[i];
-        }
-    }
-}
-void testMergeSortGood()
-{
-    int n = 10;
-    UArray<int> a = initial_list_good(n);
-    MergeSort::execute(a);
-    for (int i = 1; i <= n; i++)
-    {
-        expect(a[i - 1]).to_be(i);
-    }
-}
-void testMergeSortWorst()
-{
-    int n = 10;
-    UArray<int> a = initial_list_worst(n);
-    MergeSort::execute(a);
-    for (int i = 1; i <= n; i++)
-    {
-        expect(a[i - 1]).to_be(i);
-    }
-}
-void testMergeSortRandom()
-{
-    for (int rep = 0; rep < 10; rep++)
-    {
-        int n = rep + 10;
-        UArray<int> a = initial_list_random(n);
-        MergeSort::execute(a);
-        int prev = 0;
-        for (int i = 0; i < n; i++)
-        {
-            expect(a[i] >= prev).to_be_truthy();
-            prev = a[i];
-        }
-    }
-}
-void testQuickSortGood()
-{
-    int n = 10;
-    UArray<int> a = initial_list_good(n);
-    QuickSort::execute(a);
-    for (int i = 1; i <= n; i++)
-    {
-        expect(a[i - 1]).to_be(i);
-    }
-}
-void testQuickSortWorst()
-{
-    int n = 10;
-    UArray<int> a = initial_list_worst(n);
-    QuickSort::execute(a);
-    for (int i = 1; i <= n; i++)
-    {
-        expect(a[i - 1]).to_be(i);
-    }
-}
-void testQuickSortRandom()
-{
-    for (int rep = 0; rep < 10; rep++)
-    {
-        int n = rep + 10;
-        UArray<int> a = initial_list_random(n);
-        QuickSort::execute(a);
-        int prev = 0;
-        for (int i = 0; i < n; i++)
-        {
-            expect(a[i] >= prev).to_be_truthy();
-            prev = a[i];
-        }
-    }
-}
 
-void testInsertionSortGood()
-{
-    int n = 10;
-    UArray<int> a = initial_list_good(n);
-    InsertionSort::execute(a);
-}
-
-void testInsertionSortWorst()
-{
-    int n = 10;
-    UArray<int> a = initial_list_worst(n);
-    InsertionSort::execute(a);
-}
-
-void testInsertionSortRandom()
-{
-    for (int rep = 0; rep < 10; rep++)
+    int rep = 11;
+    n = rep + 10;
+    a = initial_list_random(n);
+    T::execute(a);
+    int prev = 0;
+    for (int i = 0; i < n; i++)
     {
-        int n = rep + 10;
-        UArray<int> a = initial_list_random(n);
-        InsertionSort::execute(a);
-        int prev = 0;
-        for (int i = 0; i < n; i++)
-        {
-            expect(a[i] >= prev).to_be_truthy();
-            prev = a[i];
-        }
-    }
-}
-
-
-void testQSortGood()
-{
-    int n = 10;
-    UArray<int> a = initial_list_good(n);
-    QSort::execute(a);
-    for (int i = 1; i <= n; i++)
-    {
-        expect(a[i - 1]).to_be(i);
-    }
-}
-
-void testQSortWorst()
-{
-    int n = 10;
-    UArray<int> a = initial_list_worst(n);
-    QSort::execute(a);
-
-    for (int i = 1; i <= n; i++)
-    {
-        expect(a[i - 1]).to_be(i);
-    }
-}
-
-void testQSortRandom()
-{
-    for (int rep = 0; rep < 10; rep++)
-    {
-        int n = rep + 10;
-        UArray<int> a = initial_list_random(n);
-        QSort::execute(a);
-        int prev = 0;
-        for (int i = 0; i < n; i++)
-        {
-            expect(a[i] >= prev).to_be_truthy();
-            prev = a[i];
-        }
+        expect(a[i] >= prev).to_be_truthy();
+        prev = a[i];
     }
 }
 
 int main()
 {
     std::cout << "TEST START" << std::endl;
-    testSelectionSortGood();
-    testSelectionSortWorst();
-    testSelectionSortRandom();
-    testMergeSortGood();
-    testMergeSortWorst();
-    testMergeSortRandom();
-    testQuickSortGood();
-    testQuickSortWorst();
-    testQuickSortRandom();
-
-    testInsertionSortGood();
-    testInsertionSortWorst();
-
-    testQSortGood();
-    testQSortWorst();
-
+    testSort<SelectionSort>();
+    testSort<MergeSort>();
+    testSort<QuickSort>();
+    testSort<InsertionSort>();
+    testSort<QSort>();
     std::cout << "ALL GREEN" << std::endl;
 }
