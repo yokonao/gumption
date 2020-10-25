6acd968bf913f0de3007e58e2abe0d6bf95d4ddb
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-25 16:22:04 +0900

    add : 1d 2dのUArrayに対するテストを追加

--- lib/tests/test_uarray.cpp
+++ lib/tests/test_uarray.cpp
@@ -2,33 +2,44 @@
 #include <expectation.h>
 #include <iostream>
 
-void testUArray()
+void testUArray1D()
+{
+    UArray<int> x;
+    x.pushBack(1);
+    expect(x.size()).to_be(1);
+    expect(x[0]).to_be(1);
+    x.pushBack(0);
+    expect(x.size()).to_be(2);
+    expect(x[1]).to_be(0);
+}
+
+void testUArray2D()
 {
     UArray<UArray<int>> x;
     UArray<int> y;
     y.pushBack(10);
-    y.pushBack(12);
+    y.pushBack(11);
     x.pushBack(y);
-    // expect(x[0][1] == 12).to_be_truthy();
-
-    std::cout << y << std::endl;
-    std::cout << x << std::endl;
-
+    expect(x[0][0] == 10).to_be_truthy();
+    expect(x[0][1] == 11).to_be_truthy();
+    expect(y[0] == 10).to_be_truthy();
+    expect(y[1] == 11).to_be_truthy();
     y.clear();
-    // expect(y.size()).to_be(0);
-    y.pushBack(10);
-    y.pushBack(12);
-    // expect(y.size()).to_be(2);
+    expect(y.size()).to_be(0);
+    y.pushBack(20);
+    y.pushBack(21);
+    expect(y.size()).to_be(2);
+    expect(x[0][0] == 10).to_be_truthy();
+    expect(x[0][1] == 11).to_be_truthy();
+    expect(y[0] == 20).to_be_truthy();
+    expect(y[1] == 21).to_be_truthy();
     y[0] = 1;
-    // expect(y[0]).to_be(1);
-    std::cout << y << std::endl;
-    std::cout << x << std::endl;
+    expect(y[0] == 1).to_be_truthy();
 }
+
 int main()
 {
-
-    std::cout << "TEST START" << std::endl;
-    testUArray();
-    std::cout << "ALL GREEN" << std::endl;
+    executeTestSuite("1次元配列のテスト", testUArray1D);
+    executeTestSuite("2次元配列のテスト", testUArray2D);
     return 0;
 }
