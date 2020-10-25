bb5a213df3e4fe53892201959f8170a294b5de07
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-25 15:52:31 +0900

    add : =

--- lib/uarray.h
+++ lib/uarray.h
@@ -24,6 +24,30 @@ public:
         assert(i >= 0 and i < n);
         return b[i];
     };
+    UArray<T> &operator=(const UArray<T> &a)
+    {
+        if (this == &a)
+            return *this;
+        if (a.w <= w)
+        {
+            for (int i = 0; i < a.n; i++)
+            {
+                b[i] = a.b[i];
+            }
+            n = a.n;
+            return *this;
+        }
+        T *bp = new T[a.w];
+        for (int i = 0; i < a.n; i++)
+        {
+            bp[i] = a.b[i];
+        }
+        delete[] b;
+        b = bp;
+        w = a.w;
+        n = a.n;
+        return *this;
+    }
     int size() const { return n; };
     void pushBack(const T &e)
     {
@@ -87,11 +111,11 @@ public:
         n = obj.n;
         w = obj.w;
     }
-    // ~UArray()
-    // {
-    //     std::cout << "del" << std::endl;
-    //     delete[] b;
-    // }
+    ~UArray()
+    {
+        std::cout << "del" << std::endl;
+        delete[] b;
+    }
 };
 template <class T>
 std::ostream &operator<<(std::ostream &os, const UArray<T> &ua)
