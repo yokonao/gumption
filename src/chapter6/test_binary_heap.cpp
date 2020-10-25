26a3f0308b3101525817d6dbde5f0d9ade6adbf8
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-25 17:51:02 +0900

    add : binaryheapのテストを追加

---
+++ src/chapter6/test_binary_heap.cpp
@@ -0,0 +1,33 @@
+#include <expectation.h>
+#include "binary_heap.h"
+
+void testBinaryHeapSimple()
+{
+    BinaryHeapPriorityQueue q;
+    q.insert(5);
+    q.insert(4);
+    q.insert(3);
+    expect(q.min()).to_be(3);
+}
+void testBinaryHeap()
+{
+    BinaryHeapPriorityQueue q;
+    q.insert(5);
+    q.insert(4);
+    q.insert(3);
+    q.insert(8);
+    q.insert(7);
+    q.insert(19);
+    q.insert(5);
+    q.insert(40);
+    q.insert(79);
+
+    expect(q.min()).to_be(3);
+}
+
+int main()
+{
+    executeTestSuite("ヒープのテストSimple", testBinaryHeapSimple);
+    executeTestSuite("ヒープのテスト", testBinaryHeap);
+    return 0;
+}
