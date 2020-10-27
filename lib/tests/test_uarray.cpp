eda2b6bf760e6fe08d4997bb72b61a410e82d549
yokonao <yokotukanao@me.com>
2020-10-27 09:02:04 +0900

    テストをリファクタ

--- lib/tests/test_uarray.cpp
+++ lib/tests/test_uarray.cpp
@@ -19,7 +19,7 @@ void testEquivalenceOperator()
     expect(x != std::array<int, 2>{1, 9}).to_be_truthy();
     expect(x != std::array<int, 2>{2, 10}).to_be_truthy();
 
-    expect(x).to_be_equal_to(std::array<int, 2>{1,  10});
+    expect(x).to_be_equal_to(std::array<int, 2>{1, 10});
 }
 
 void testUArray1D()
@@ -40,21 +40,22 @@ void testUArray2D()
     y.pushBack(10);
     y.pushBack(11);
     x.pushBack(y);
-    expect(x[0][0] == 10).to_be_truthy();
-    expect(x[0][1] == 11).to_be_truthy();
-    expect(y[0] == 10).to_be_truthy();
-    expect(y[1] == 11).to_be_truthy();
+    
+    expect(x[0]).to_be_equal_to(std::array<int, 2>{10, 11});
+    expect(y).to_be_equal_to(std::array<int, 2>{10, 11});
+
     y.clear();
     expect(y.size()).to_be(0);
+    
     y.pushBack(20);
     y.pushBack(21);
     expect(y.size()).to_be(2);
-    expect(x[0][0] == 10).to_be_truthy();
-    expect(x[0][1] == 11).to_be_truthy();
-    expect(y[0] == 20).to_be_truthy();
-    expect(y[1] == 21).to_be_truthy();
+    expect(y).to_be_equal_to(std::array<int, 2>{20, 21});
+    expect(x[0]).to_be_equal_to(std::array<int, 2>{10, 11});
+
     y[0] = 1;
     expect(y[0] == 1).to_be_truthy();
+    expect(y).to_be_equal_to(std::array<int, 2>{1, 21});
 }
 
 int main()
