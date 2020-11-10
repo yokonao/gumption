5da05aa98426e95481c880fbe36de8a93c3697f8
yokonao <yokotukanao@me.com>
2020-11-10 21:11:54 +0900

    add: 双方向連結リストの列構造を実装

--- lib/doubly_linked_list.h
+++ lib/doubly_linked_list.h
@@ -1,6 +1,8 @@
 #ifndef DOUBLY_LIKED_LIST_HPP
 #define DOUBLY_LIKED_LIST_HPP
 
+#include <cassert>
+
 template <typename T>
 class DoublyLinkedItem
 {
@@ -24,6 +26,74 @@ public:
         this->prev = prev;
         this->next = next;
         isDummy = false;
+    }
+};
+
+template <typename T>
+class DoublyLinkedList
+{
+private:
+    DoublyLinkedItem<T> _head;
+
+    void splice(DoublyLinkedItem<T> *a, DoublyLinkedItem<T> *b, DoublyLinkedItem<T> *t)
+    {
+        // aがvalidなLinkedListに所属しているか
+        DoublyLinkedItem<T> *tmp = a->next;
+        while (!tmp->isDummy)
+        {
+            assert(tmp != a);
+            tmp = tmp->next;
+        }
+        // 同リスト内に[a...b]が存在し、tが含まれない
+        tmp = a;
+        while (tmp != b)
+        {
+            assert(!tmp->isDummy);
+            assert(tmp != t);
+            tmp = tmp->next;
+        }
+
+        DoublyLinkedItem<T> *ap = a->prev;
+        DoublyLinkedItem<T> *bp = b->next;
+        ap->next = bp;
+        bp->prev = ap;
+
+        DoublyLinkedItem<T> *tp = t->next;
+        b->next = tp;
+        a->prev = t;
+        t->next = a;
+        tp->prev = b;
+    };
+
+public:
+    DoublyLinkedList()
+    {
+    }
+
+    ~DoublyLinkedList()
+    {
+    }
+
+    DoublyLinkedItem<T> *head()
+    {
+        return &_head;
+    }
+
+    bool isEmpty()
+    {
+        return &_head == _head.next;
+    }
+
+    DoublyLinkedItem<T> *first()
+    {
+        assert(!isEmpty());
+        return _head.next;
+    }
+
+    DoublyLinkedItem<T> *last()
+    {
+        assert(!isEmpty());
+        return _head.prev;
     }
 };
 
