3bab916b97ac8076aab1c791ef830dbc8d4cf574
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-12 20:33:08 +0900

    add : insertとlocate

--- src/chapter7/binary_search_tree.cpp
+++ src/chapter7/binary_search_tree.cpp
@@ -1,62 +1,121 @@
 #include "binary_search_tree.h"
+#include <iostream>
 
-KeyAndValue::KeyAndValue() {}
-
-KeyAndValue::KeyAndValue(int key, std::string value)
+Dict::Dict()
+{
+    this->key = 0;
+    this->value = "";
+}
+Dict::Dict(int key, std::string value)
 {
     this->key = key;
     this->value = value;
 }
+Dict::~Dict() {}
+std::ostream &operator<<(std::ostream &os, const Dict &d)
+{
+    os << "(" << d.key << ", ";
+    os << d.value << ") ";
+    return os;
+}
+BinarySearchLeaf::BinarySearchLeaf() {}
 
-BinarySearchLeafNode::BinarySearchLeafNode() {}
+BinarySearchLeaf::~BinarySearchLeaf() {}
 
-BinarySearchLeafNode::~BinarySearchLeafNode() {}
-
-BinarySearchLeafNode::BinarySearchLeafNode(int splitter, BinarySearchLeafNode *left, BinarySearchLeafNode *right)
+BinarySearchLeaf::BinarySearchLeaf(int splitter, BinarySearchLeaf *left, BinarySearchLeaf *right)
 {
     this->splitter = splitter;
-    this->leftNode = left;
-    this->rightNode = right;
+    this->leftLeaf = left;
+    this->rightLeaf = right;
 }
 
-BinarySearchLeafNode::BinarySearchLeafNode(int splitter, DoublyLinkedItem<KeyAndValue> *left, BinarySearchLeafNode *right)
+BinarySearchLeaf::BinarySearchLeaf(DoublyLinkedItem<Dict> *dict)
 {
-    this->splitter = splitter;
-    this->leftItem = left;
-    this->rightNode = right;
-    this->toLeftIsLowest = true;
+    this->isItem = true;
+    this->_d = dict;
 }
 
-BinarySearchLeafNode::BinarySearchLeafNode(int splitter, BinarySearchLeafNode *left, DoublyLinkedItem<KeyAndValue> *right)
+int BinarySearchLeaf::insertRec(Dict dict, DoublyLinkedList<Dict> *list)
 {
-    this->splitter = splitter;
-    this->leftNode = left;
-    this->rightItem = right;
-    this->toRightIsLowest = true;
-}
-
-BinarySearchLeafNode::BinarySearchLeafNode(int splitter, DoublyLinkedItem<KeyAndValue> *left, DoublyLinkedItem<KeyAndValue> *right)
-{
-    this->splitter = splitter;
-    this->leftItem = left;
-    this->rightItem = right;
-    this->toLeftIsLowest = true;
-    this->toRightIsLowest = true;
-}
-
-BinarySearchTree::BinarySearchTree() {}
-
-BinarySearchTree::~BinarySearchTree() {}
-
-void BinarySearchTree::insert(KeyAndValue tuple)
-{
-    if (isEmpty)
+    if (isItem)
     {
-        DoublyLinkedItem<KeyAndValue> *node = _list.insertAfter(tuple, _list.head());
-        _root = new BinarySearchLeafNode(tuple.key, node, _list.head());
-        isEmpty = false;
+        isItem = false;
+        if (dict.key <= _d->body.key)
+        {
+            DoublyLinkedItem<Dict> *dictItem = list->insertBefore(dict, _d);
+            splitter = dict.key;
+            leftLeaf = new BinarySearchLeaf(dictItem);
+            rightLeaf = new BinarySearchLeaf(_d);
+        }
+        else
+        {
+            splitter = _d->body.key;
+            DoublyLinkedItem<Dict> *dictItem = list->insertAfter(dict, _d);
+            leftLeaf = new BinarySearchLeaf(_d);
+            rightLeaf = new BinarySearchLeaf(dictItem);
+        }
+        return 1;
     }
     else
     {
+        if (dict.key <= splitter)
+        {
+            return 1 + leftLeaf->insertRec(dict, list);
+        }
+        else
+        {
+            return 1 + rightLeaf->insertRec(dict, list);
+        }
     }
+}
+
+Dict BinarySearchLeaf::locateRec(int key)
+{
+    if (isItem)
+    {
+        return _d->body;
+    }
+    else
+    {
+        if (key <= splitter)
+        {
+            return leftLeaf->locateRec(key);
+        }
+        else
+        {
+            return rightLeaf->locateRec(key);
+        }
+    }
+}
+
+BinarySearchTree::BinarySearchTree()
+{
+    // key:infinityのNodeを作成
+    Dict _d(10000000, ""); //todo:infinityで置き換える
+    DoublyLinkedItem<Dict> *item = _list.insertAfter(_d, _list.head());
+    _root = new BinarySearchLeaf(item);
+}
+
+BinarySearchTree::~BinarySearchTree() {}
+
+void BinarySearchTree::insert(Dict dict)
+{
+    //DoublyLinkedItem<Dict> *item = _list.insertAfter(d, _list.head());
+    //_root = new BinarySearchLeaf(d.key, item, _list.head());
+    //isEmpty = false;
+    if (_root->insertRec(dict, &_list) > hight)
+    {
+        hight++;
+    };
+}
+
+Dict BinarySearchTree::locate(int key)
+{
+    return _root->locateRec(key);
+}
+
+void BinarySearchTree::Print()
+{
+    std::cout << "DoublyLinkedList:" << _list << std::endl;
+    //std::cout << "BinarySearchTree:" << _root->splitter << std::endl;
 }
