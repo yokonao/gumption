0d7f43346fc25b359d7c50ad92733fe403cd33b3
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-12 22:48:36 +0900

    add : removeのテストを追加

--- src/chapter7/test_binary_search_tree.cpp
+++ src/chapter7/test_binary_search_tree.cpp
@@ -68,6 +68,62 @@ void testPrint()
     t.Print();
 }
 
+void testBinarySearchTreeRemove1()
+{
+    BinarySearchTree t;
+    t.insert(Dict(1, "a"));
+    t.insert(Dict(2, "b"));
+    t.insert(Dict(5, "e"));
+    t.insert(Dict(4, "d"));
+    t.insert(Dict(3, "c"));
+    t.insert(Dict(6, "f"));
+
+    t.Print();
+    t.remove(6);
+    t.Print();
+    expect(t.locate(0).value).to_be("a");
+    expect(t.locate(1).value).to_be("a");
+    expect(t.locate(2).value).to_be("b");
+    expect(t.locate(3).value).to_be("c");
+    expect(t.locate(4).value).to_be("d");
+    expect(t.locate(5).value).to_be("e");
+    expect(t.height()).to_be(5);
+
+    t.insert(Dict(6, "g"));
+    t.Print();
+    t.remove(4);
+    t.Print();
+    expect(t.locate(0).value).to_be("a");
+    expect(t.locate(1).value).to_be("a");
+    expect(t.locate(2).value).to_be("b");
+    expect(t.locate(3).value).to_be("c");
+    expect(t.locate(5).value).to_be("e");
+    expect(t.locate(6).value).to_be("g");
+
+    expect(t.locate(7).value).to_be("");
+    expect(t.height()).to_be(4);
+}
+
+void testBinarySearchTreeRemove2()
+{
+    BinarySearchTree t;
+    t.insert(Dict(10, "e"));
+    t.insert(Dict(2, "a"));
+    t.insert(Dict(3, "b"));
+    t.insert(Dict(4, "c"));
+    t.insert(Dict(5, "d"));
+    t.Print();
+    t.remove(10);
+    t.Print();
+
+    expect(t.locate(2).value).to_be("a");
+    expect(t.locate(3).value).to_be("b");
+    expect(t.locate(4).value).to_be("c");
+    expect(t.locate(5).value).to_be("d");
+
+    expect(t.height()).to_be(4);
+}
+
 int main()
 {
     executeTestSuite("葉のテストSimple", testBinarySearchLeafSimple);
@@ -76,6 +132,8 @@ int main()
     executeTestSuite("二分木のInsertテストSimple", testBinarySearchTreeInsertSimple);
     executeTestSuite("二分木のInsertテストNormal1", testBinarySearchTreeInsertNormal1);
     executeTestSuite("二分木のInsertテストNormal2", testBinarySearchTreeInsertNormal2);
+    executeTestSuite("二分木のRemoveテスト1", testBinarySearchTreeRemove1);
+    executeTestSuite("二分木のRemoveテスト2", testBinarySearchTreeRemove2);
 
     return 0;
 }
