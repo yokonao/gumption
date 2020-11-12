1545f0ed25f0e57f179ea8c87340ee328c9029d9
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-12 15:27:05 +0900

    add : simpleなテスト

---
+++ src/chapter7/test_binary_search_tree.cpp
@@ -0,0 +1,13 @@
+#include <expectation.h>
+#include "binary_search_tree.h"
+
+void testBinarySearchLeafSimple()
+{
+    BinarySearchLeafNode t;
+}
+
+int main()
+{
+    executeTestSuite("二分木のテストSimple", testBinarySearchLeafSimple);
+    return 0;
+}
