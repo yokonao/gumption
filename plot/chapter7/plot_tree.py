84ca7c0117ba7fe0751c5b0f6c055d2922992068
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-18 22:23:11 +0900

    update : 不要な変数を削除

--- plot/chapter7/plot_tree.py
+++ plot/chapter7/plot_tree.py
@@ -7,11 +7,10 @@ def main():
     base_dir = os.environ["GUMPTION"]
     data_dir = os.path.join(base_dir, "results/chapter7")
     with open(os.path.join(data_dir, "out.json"), "r") as f:
-        json_open = json.load(f)
+        json_open = json.load(f)["BinarySearchTree"]
     graph = Digraph(format="png")
     relation_ls = []
-    data_ls = json_open["BinarySearchTree"]
-    for data in data_ls:
+    for data in json_open:
         if len(data) == 0:
             break
         if data["isItem"]:
