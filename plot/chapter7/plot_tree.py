8548ca2894ad2e14c9f56a01539b561df78be440
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-18 22:58:53 +0900

    add : ファイルを複数受け取って描画

--- plot/chapter7/plot_tree.py
+++ plot/chapter7/plot_tree.py
@@ -1,28 +1,29 @@
 import os
 import json
+import glob
 from graphviz import Digraph
 
 
 def main():
     base_dir = os.environ["GUMPTION"]
     data_dir = os.path.join(base_dir, "results/chapter7")
-    with open(os.path.join(data_dir, "out.json"), "r") as f:
-        json_open = json.load(f)["BinarySearchTree"]
-    graph = Digraph(format="png")
-    relation_ls = []
-    for data in json_open:
-        if len(data) == 0:
-            break
-        if data["isItem"]:
-            graph.node(data["id"], str(data["key"]), shape="box")
-        else:
-            graph.node(data["id"], str(data["key"]))
-            relation_ls.append((data["id"], data["left"]))
-            relation_ls.append((data["id"], data["right"]))
-    for relation in relation_ls:
-        graph.edge(relation[0], relation[1])
-
-    graph.render(os.path.join(data_dir, "tree"))
+    for fname in glob.glob(os.path.join(data_dir, "*.json")):
+        with open(fname, "r") as f:
+            json_open = json.load(f)["BinarySearchTree"]
+        graph = Digraph(format="png")
+        relation_ls = []
+        for data in json_open:
+            if len(data) == 0:
+                break
+            if data["isItem"]:
+                graph.node(data["id"], str(data["key"]), shape="box")
+            else:
+                graph.node(data["id"], str(data["key"]))
+                relation_ls.append((data["id"], data["left"]))
+                relation_ls.append((data["id"], data["right"]))
+        for relation in relation_ls:
+            graph.edge(relation[0], relation[1])
+        graph.render(os.path.splitext(fname)[0])
 
 
 if __name__ == "__main__":
