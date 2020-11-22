739cf7107a2157c5e4d9453c05951ee3d9a0ad8c
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-22 17:57:16 +0900

    add : atoiのテスト

--- lib/tests/test_string_operation.cpp
+++ lib/tests/test_string_operation.cpp
@@ -36,10 +36,34 @@ void testInplaceReverse()
     expect(charcmp(r, p)).to_be_truthy();
 }
 
+void testAtoi()
+{
+    char a[7] = "123456";
+    char b[8] = "+123456";
+    char c[8] = "-123456";
+    char d[11] = "12Hoge3456";
+    char e[11] = "    123456";
+    char f[17] = "\t\n\v\f\r123456";
+    char g[11] = "0000123456";
+    char h[12] = "-2147483648";
+    char i[11] = "2147483647";
+
+    expect(f_atoi(a)).to_be(123456);
+    expect(f_atoi(b)).to_be(123456);
+    expect(f_atoi(c)).to_be(-123456);
+    expect(f_atoi(d)).to_be(12);
+    expect(f_atoi(e)).to_be(123456);
+    expect(f_atoi(f)).to_be(123456);
+    expect(f_atoi(g)).to_be(123456);
+    expect(f_atoi(h)).to_be(-2147483648);
+    expect(f_atoi(i)).to_be(2147483647);
+}
+
 int main()
 {
     executeTestSuite("strlenのテスト", testStrlen);
     executeTestSuite("charcmpのテスト", testCharCmp);
     executeTestSuite("反転のテスト", testInplaceReverse);
+    executeTestSuite("atoiのテスト", testAtoi);
     return 0;
 }
