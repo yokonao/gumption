b57c3f10eddde9e9d8a3a17f6e6799a78428f4d0
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-12-06 09:54:50 +0900

    add : test

--- src/chapter9/test_graph.cpp
+++ src/chapter9/test_graph.cpp
@@ -38,10 +38,38 @@ void testGraphNext()
     expect(v).to_be_equal_to(UArray<int>{1, 2});
     expect(w).to_be_equal_to(UArray<int>{0, 4});
 }
+void testGraphDefaultConstructer()
+{
+    Graph g;
+    expect(g.size()).to_be(0);
+}
+void testGraphCopyConstructer()
+{
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(3, 1);
+    g.addEdge(2, 4);
+    Graph h = g;
+    g.addEdge(0, 3);
+    UArray<int> v = g.next(0);
+    UArray<int> w = g.next(2);
+
+    expect(v).to_be_equal_to(UArray<int>{1, 2, 3});
+    expect(w).to_be_equal_to(UArray<int>{0, 4});
+
+    UArray<int> x = h.next(0);
+    UArray<int> y = h.next(2);
+
+    expect(x).to_be_equal_to(UArray<int>{1, 2});
+    expect(y).to_be_equal_to(UArray<int>{0, 4});
+}
 int main()
 {
     executeTestSuite("グラフのテストSimple", testGraphSimple);
     executeTestSuite("グラフのテストNormal", testGraphNormal);
     executeTestSuite("nextのテスト", testGraphNext);
+    executeTestSuite("DefaultConstructerのテスト", testGraphDefaultConstructer);
+    executeTestSuite("CopyConstructrのテスト", testGraphCopyConstructer);
     return 0;
 }
