21351585a64e208d6ccb51cdf014d5b97ba951a8
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-12-10 21:27:24 +0900

    add : FIFOを使ったBFSのテスト

--- src/chapter9/test_graph.cpp
+++ src/chapter9/test_graph.cpp
@@ -79,6 +79,18 @@ void testBreadthFirstSearch()
     expect(d).to_be_equal_to(Array<int>{0, 1, 1, 2, 2});
 }
 
+void testBreadthFirstSearchQueue()
+{
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(3, 4);
+    g.addEdge(2, 4);
+    Array<int> d = g.bfs_queue(0);
+    expect(d).to_be_equal_to(Array<int>{0, 1, 1, 2, 2});
+}
+
 void testDepthFirstSearchStack()
 {
     Graph g(5);
@@ -99,6 +111,7 @@ int main()
     executeTestSuite("グラフのテストNormal", testGraphNormal);
     executeTestSuite("nextのテスト", testGraphNext);
     executeTestSuite("幅優先探索のテスト", testBreadthFirstSearch);
+    executeTestSuite("幅優先探索Queueのテスト", testBreadthFirstSearchQueue);
     executeTestSuite("深さ優先探索Stackのテスト", testDepthFirstSearchStack);
     return 0;
 }
